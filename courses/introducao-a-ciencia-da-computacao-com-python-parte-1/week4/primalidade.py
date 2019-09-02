27fb9b7cbbc7455132e007584c1f22c5fe8fc3d2
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2019-09-01 22:42:58 -0300

    Exercícios da semana 4 - Parte III - Ajustes nos comentários e nomeclatura de variável

--- courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week4/primalidade.py
+++ courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week4/primalidade.py
@@ -10,14 +10,14 @@
 # Verifica se um número é primo.
 # OBS: Um número primo possui apenas dois divisores: 1 e ele mesmo. Esta função
 # cria um laço de repetição baseada no número recebido. A cada ciclo do loop
-# é realizado uma divisão sem resto. A cada divisão sem resto é a variável
+# uma divisão sem resto é realizada. Em caso de sucesso, a variável
 # numeroDeDivisores é incrementada. Quando esta variável se torna maior que 2,
 # detectamos que o número não é primo
 def EsteNumeroEPrimo(numero):
     numeroDeDivisores = 0
 
-    for x in range(1, numero + 1):
-        if numero % x == 0:
+    for divisor in range(1, numero + 1):
+        if numero % divisor == 0:
             numeroDeDivisores += 1
 
             if numeroDeDivisores > 2:
