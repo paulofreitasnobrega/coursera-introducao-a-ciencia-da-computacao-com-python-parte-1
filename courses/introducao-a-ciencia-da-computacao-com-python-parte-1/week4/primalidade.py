884ce256c8116b2e9df01f95e75a0561bdee9ef9
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2019-09-01 22:38:48 -0300

    Exercícios da semana 4 - Parte III

--- courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week4/primalidade.py
+++ courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week4/primalidade.py
@@ -7,4 +7,23 @@
 
 # Aluno: Paulo Freitas Nobrega
 
-#https://www.somatematica.com.br/fundam/primos.php
+# Verifica se um número é primo.
+# OBS: Um número primo possui apenas dois divisores: 1 e ele mesmo. Esta função
+# cria um laço de repetição baseada no número recebido. A cada ciclo do loop
+# é realizado uma divisão sem resto. A cada divisão sem resto é a variável
+# numeroDeDivisores é incrementada. Quando esta variável se torna maior que 2,
+# detectamos que o número não é primo
+def EsteNumeroEPrimo(numero):
+    numeroDeDivisores = 0
+
+    for x in range(1, numero + 1):
+        if numero % x == 0:
+            numeroDeDivisores += 1
+
+            if numeroDeDivisores > 2:
+                return False
+
+    return True
+
+numero = int(input("Forneça um número inteiro positivo: "))
+print("primo" if EsteNumeroEPrimo(numero) else "não primo")
