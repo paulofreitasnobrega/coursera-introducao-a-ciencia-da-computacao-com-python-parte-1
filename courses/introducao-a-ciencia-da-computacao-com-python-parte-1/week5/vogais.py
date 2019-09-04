05efd2c17219762550567f13b9c602bd724bdc1f
Paulo Freitas Nobrega <paulofreitasnobrega@gmail.com>
2019-09-04 14:56:58 -0300

    Exercícios da Semana 5 - Parte II

---
+++ courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week5/vogais.py
@@ -0,0 +1,25 @@
+# Week 5 - Lista de Exercícios 4
+# Exercício 3 - Vogais
+
+# Escreva a função vogal que recebe um único caractere como parâmetro e
+# devolve True se ele for uma vogal e False se for uma consoante.
+
+#Aluno: Paulo Freitas Nobrega
+
+def vogal(caractere):
+    if isinstance(caractere, str):
+        vogais = ['a', 'e', 'i', 'o', 'u']
+        return caractere in vogais
+    return False
+
+# Realiza o teste com vogais
+def test_vogais():
+    assert vogal("a") == True
+
+# Realiza o teste com consoantes
+def test_consoantes():
+    assert vogal("b") == False
+
+# Realiza o teste com valores diferentes de string
+def test_NaoString():
+    assert vogal(1) == False
