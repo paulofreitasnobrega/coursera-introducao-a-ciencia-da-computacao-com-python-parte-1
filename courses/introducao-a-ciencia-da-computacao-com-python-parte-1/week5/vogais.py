ade6bbaa93dffea1bffae8d2a17694d78dfdd1b2
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2019-09-04 19:40:01 -0300

    Corrigido Bug que fazia a função vogal falhar ao receber caracteres maiúsculos

--- courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week5/vogais.py
+++ courses/introducao-a-ciencia-da-computacao-com-python-parte-1/week5/vogais.py
@@ -8,7 +8,7 @@
 
 def vogal(caractere):
     if isinstance(caractere, str):
-        vogais = ['a', 'e', 'i', 'o', 'u']
+        vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         return caractere in vogais
     return False
 
